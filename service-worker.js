6e1be2d7bef78552e5b3d055573560e13b6fd3ca
Alisa <alissa9090@mail.ru>
2020-09-09 11:19:59 +0200

    cache all files and rebuild

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,16 @@
 const CACHE_NAME = 'test0.3';
 const FILES_TO_CACHE = [
-  './'
+  './',
+  './manifest.webmanifest',
+  './favicon.d42470cd.ico',
+  './shopping-list-128x128.53711125.png',
+  './shopping-list-144x144.8c812167.png',
+  './shopping-list-152x152.9ce296dc.png',
+  './shopping-list-192x192.a8be18ee.png',
+  './shopping-list-256x256.24932ea0.png',
+  './shopping-list-512x512.29e19655.png',
+  './src.fc45d0fd.css',
+  './src.fc45d0fd.js'
 ];
 
 self.addEventListener('install', (evt) => {
@@ -44,13 +54,6 @@ self.addEventListener('fetch', (evt) => {
     caches.match(evt.request).then( response => {
       return response || fetch(evt.request);
     })
-      // fetch(evt.request)
-      //     .catch(() => {
-      //       return caches.open(CACHE_NAME)
-      //           .then((cache) => {
-      //             return cache.match('offline.html');
-      //           });
-      //     })
   );
 });
 
