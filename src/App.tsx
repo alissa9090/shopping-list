3bf1728d7b5d3c88fc8676f563a6f6a00e52db74
Alisa <alissa9090@mail.ru>
2020-09-22 11:31:41 +0200

    setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react'
+import React from 'react';
 import StoreProvider from './stores/context';
 import Shops from './components/shops';
 import ButtonAppBar from './components/appBar';
 import '../assets/css/App.less';
 import Palette from './components/base/Palette';
 
-const App: React.FC = () => {
-  return (
-    <Palette>
-      <StoreProvider>
-        <div className="App">
-          <div className="app_sector header">
-            <ButtonAppBar/>
-          </div>
-          <div className="app_sector shops">
-            <Shops />
-          </div>
+const App: React.FC = () => (
+  <Palette>
+    <StoreProvider>
+      <div className="App">
+        <div className="app_sector header">
+          <ButtonAppBar />
         </div>
-      </StoreProvider>
-    </Palette>
-  );
-}
+        <div className="app_sector shops">
+          <Shops />
+        </div>
+      </div>
+    </StoreProvider>
+  </Palette>
+);
 
 export default App;
